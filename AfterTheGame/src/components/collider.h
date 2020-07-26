5c42a029999f8b5e0020f1e0dfa416787ed5b354
Daniil Blagiy <blagiy.daniil@gmail.com>
2020-07-26 11:15:04 +0300

    added an offset for hitbox in collider component

--- AfterTheGame/src/components/collider.h
+++ AfterTheGame/src/components/collider.h
@@ -12,12 +12,17 @@ struct ColliderComponent : public ecs::Component
 {
 	sf::FloatRect rect;
 	std::string tag;
+	sf::Vector2f offset;	// an offset of hitbox position relative to the transform component position
 
 	TransformComponent* transform_component;
 
 
 	ColliderComponent(float width, float height, std::string s_tag)
-		: rect(0.0f, 0.0f, width, height), tag(s_tag)
+		: rect(0.0f, 0.0f, width, height), tag(s_tag), offset(0.0f, 0.0f)
+	{}
+
+	ColliderComponent(float width, float height, std::string s_tag, sf::Vector2f s_offset)
+		: rect(0.0f, 0.0f, width, height), tag(s_tag), offset(s_offset)
 	{}
 
 	void init() override
@@ -32,7 +37,7 @@ struct ColliderComponent : public ecs::Component
 
 	void update(float delta) override
 	{
-		rect.left = transform_component->position.x;
-		rect.top = transform_component->position.y;
+		rect.left = transform_component->position.x + offset.x;
+		rect.top = transform_component->position.y + offset.y;
 	}
 };
